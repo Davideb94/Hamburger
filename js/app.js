ee0acebc2152f15ce8ac347517165df93968e39c
Davide Brunetti <davide.brunetti@novetica.org>
2016-09-21 02:59:34 +0100

    First commit.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+	var button = document.getElementById("container");
+	var animated = false;
+	
+	function animate(){
+		if(animated==false){
+			button.className = "animate";
+			animated = true;
+		}
+		else{
+			button.className = "";
+			animated = false;
+		}
+	}
+
+	button.addEventListener("click", animate);
+}
